630c35353f62391e6ba3c31f05aa827a9444f822
LanceYang <lanceYang15@gmail.com>
2018-02-27 09:04:36 -0600

    new class and added new git ignore

---
+++ GitIgnoreTest/GitIgnoreTest/Hide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIgnoreTest
+{
+    class Hide
+    {
+        public Hide()
+        {
+
+        }
+
+        public void IsHidden()
+        {
+            Console.WriteLine("this should be hidden from github!");
+        }
+    }
+}
